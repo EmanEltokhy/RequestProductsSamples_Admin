367c36dfe4d35c7163306d515531dde2d6d3dfba
EmanEltokhy <emaneltokhy6@gamil.com>
2021-09-28 23:01:13 +0200

    Initial commit

--- app/src/main/java/com/example/productssamples/MainActivity.kt
+++ app/src/main/java/com/example/productssamples/MainActivity.kt
@@ -3,11 +3,9 @@ package com.example.productssamples
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
-import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
-import java.util.regex.Matcher
-import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
     private lateinit var email: TextInputEditText
@@ -21,25 +19,25 @@ class MainActivity : AppCompatActivity() {
         login = findViewById(R.id.btn_login)
 
         login.setOnClickListener {
-            if(email.text.toString().trim().length ==0){
-                email.setError("email is empty");
-                email.requestFocus();
+            if(email.text.toString().trim().isEmpty()){
+                email.error= "Email is Empty"
+                email.requestFocus()
             }
 
-            else if (!isEmailValid(email.text.toString().trim()))
+            else if (Patterns.EMAIL_ADDRESS.matcher(email.text).matches().not())
             {
-                email.setError("email not match as pattern email");
-                email.requestFocus();
+                email.error = "Enter Correct Format"
+                email.requestFocus()
 
             }
-            else if(password.text.toString().trim().length==0){
-                password.setError("Password is empty");
-                password.requestFocus();
+            else if(password.text.toString().trim().isEmpty()){
+                password.error = "Password is Empty"
+                password.requestFocus()
             }
             else if (password.text.toString().trim().length < 8)
             {
-                password.setError("Password should be more than 8 digit");
-                password.requestFocus();
+                password.error = "Password should be more than 8 digit"
+                password.requestFocus()
 
             }
             else
@@ -48,15 +46,6 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
 
             }
-
-
         }
     }
-    fun  isEmailValid( email : String) : Boolean {
-        var expression : String = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$"
-        var pattern : Pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
-        var   matcher : Matcher = pattern.matcher(email);
-        return matcher.matches()
-    }
-
 }
