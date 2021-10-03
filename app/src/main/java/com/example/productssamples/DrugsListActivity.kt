4b7ba866871305bdfaf2753f0b8721eebd7524ab
EmanEltokhy <emaneltokhy6@gamil.com>
2021-10-03 03:10:51 +0200

    -

--- app/src/main/java/com/example/productssamples/DrugsListActivity.kt
+++ app/src/main/java/com/example/productssamples/DrugsListActivity.kt
@@ -2,28 +2,37 @@ package com.example.productssamples
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.AssetManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.SearchView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.productssamples.Adapter.DrugListAdapter
 import com.example.productssamples.Model.DrugsModel
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
-import java.io.File
-import java.io.InputStream
+import java.io.*
 import java.util.*
-
+import java.util.jar.Manifest
+private const val FILE_REQUEST_CODE=5
+private const val READ_REQUEST_CODE=10
+private const val MANAGE_REQUEST_CODE=15
 class DrugsListActivity : AppCompatActivity() {
     var drugs =  mutableListOf<DrugsModel>()
     var displaydrugs =  mutableListOf<DrugsModel>()
-    var path:String="h"
+    var filepath:String=""
     private lateinit var recycler:RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,33 +59,66 @@ class DrugsListActivity : AppCompatActivity() {
         recycler.layoutManager = GridLayoutManager(this,2)
         recycler.adapter = adapter
         findViewById<Button>(R.id.upload_excel).setOnClickListener {
+            if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             var i = Intent()
             i.type = "text/*"
             i.setAction(Intent.ACTION_GET_CONTENT)
-            /*var i: Intent = Intent(Intent.ACTION_GET_CONTENT)
-            i.addCategory(Intent.CATEGORY_OPENABLE)
-            i.type = "text/csv"*/
-            startActivityForResult(Intent.createChooser(i,"Open CSV"),5)
-
-/*            var file:File = File(path)
-            val lines:List<List<String>> = csvReader().readAll(file)
-            Toast.makeText(this@DrugsListActivity, lines.toString(), Toast.LENGTH_SHORT).show()*/
-
-
+            startActivityForResult(Intent.createChooser(i,"Open CSV"), FILE_REQUEST_CODE)
+            } else {
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),READ_REQUEST_CODE)
+            }
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode==5 && resultCode == Activity.RESULT_OK)
+        if(requestCode== FILE_REQUEST_CODE && resultCode == RESULT_OK)
         {
             data?.let {
-                path = data.data.toString()
-                Toast.makeText(this@DrugsListActivity, path, Toast.LENGTH_SHORT).show()
-
+                var path = data.data.toString()
+                filepath = path.substring(path.lastIndexOf("%")+3)
+                accessFIle()
             }
         }
     }
+
+    private fun accessFIle() {
+
+            val sb = StringBuilder()
+            var textfile = File(Environment.getExternalStorageDirectory(), filepath)
+            var fis = FileInputStream(textfile)
+
+            if (fis != null) {
+                var isr = InputStreamReader(fis)
+                var buffer = BufferedReader(isr)
+
+                var line: String? = null
+                while (buffer.readLine()?.also { line = it } != null) {
+                    sb.append(line + "\n")
+                }
+                fis.close()
+            }
+            Toast.makeText(this@DrugsListActivity, sb.toString(), Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode== READ_REQUEST_CODE && grantResults[0]==PackageManager.PERMISSION_GRANTED) {
+
+            val i = Intent()
+            i.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+            startActivityForResult(i,MANAGE_REQUEST_CODE)
+
+        }else if(requestCode==MANAGE_REQUEST_CODE && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            accessFIle()
+        else
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.drugslist_menu,menu)
         menu?.findItem(R.id.search)?.let {
